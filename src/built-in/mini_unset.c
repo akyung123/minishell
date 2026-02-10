0e37b29a0727ace09f80d867d82f064334e3fd09
akyung123 <kik081212@gmail.com>
2026-02-10 20:14:56 +0900

    builtin : implement unset

--- src/built-in/mini_unset.c
+++ src/built-in/mini_unset.c
@@ -1,28 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   mini_unset.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akkim <akkim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/07 15:02:03 by akkim             #+#    #+#             */
-/*   Updated: 2026/02/10 20:12:08 by akkim            ###   ########.fr       */
+/*   Created: 2026/02/10 19:04:34 by akkim             #+#    #+#             */
+/*   Updated: 2026/02/10 20:14:18 by akkim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "env.h"
 
-int	main(int argc, char **argv, char **envp)
+static int	chk_key(t_info_env *env, char *key)
 {
-	t_info_env	env;
+	if (ft_isalpha(key[0]) || key[0] == '_')
+	{
+		env->exit_code = 1;
+		return (1);
+	}
+	return (0);
+}
 
-	(void)argc;
-	(void)argv;
-	init_env(&env, envp);
-	mini_export(&env, "name=akkim");
-	ft_printf("%s\n", get_env_val(env.head, "name"));
-	mini_unset(&env, "name");
-	ft_printf("%s\n", get_env_val(env.head, "name"));
-	all_free_env(env.head);
+// remove node for key and free node
+void	mini_unset(t_info_env *env, char *key)
+{
+	t_env	*node;
+
+	if (!key)
+	{
+		env->exit_code = 0;
+		return ;
+	}
+	if (!chk_key(env, key))
+		return ;
+	node = find_env_node(env->head, key);
+	if (node != NULL)
+		remove_env(env, key);
+	return ;
 }
