8575ec55238e373fcab917cb8486a46c0e5dba5e
akyung123 <kik081212@gmail.com>
2026-02-28 00:01:03 +0900

    feat: add parser struct definitions

--- include/parsing.h
+++ include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: akkim <akkim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 18:00:44 by akkim             #+#    #+#             */
-/*   Updated: 2026/02/27 15:26:56 by akkim            ###   ########.fr       */
+/*   Updated: 2026/02/28 00:01:03 by akkim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ typedef struct l_simple_command
 {
 	char		*cmd;
 	char		**args;
-	t_redirect	*redirect;
+	t_redirect	*pre_red;
+	t_redirect	*suff_red;
 
 }	t_simple_command;
 
@@ -42,7 +43,9 @@ typedef struct l_command_line
 	struct l_command_line	*next;	// 오른쪽 코멘트 라인 ** 존재 or 'None'** None되면 line 종료
 }	t_command_line;
 
-
+char	*ft_strrchr(const char *str, int ch);
+char	*ft_strstr(const char *haystack, const char *needle);
+"나 '에 대해서 검사
 void	builin_handler(t_info_env *env, t_simple_command *simple_command);
 t_command_line	*passing_command_line(char *line);
 
