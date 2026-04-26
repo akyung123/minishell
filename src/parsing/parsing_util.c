015e2b9430dad6ad5b5a577969e6f1ec9a1a4e35
akyung123 <kik081212@gmail.com>
2026-04-27 08:01:52 +0900

    refactor: comply with norminette rules

--- src/parsing/parsing_util.c
+++ src/parsing/parsing_util.c
@@ -6,7 +6,7 @@
 /*   By: akkim <akkim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/27 15:27:34 by akkim             #+#    #+#             */
-/*   Updated: 2026/03/19 17:56:00 by akkim            ###   ########.fr       */
+/*   Updated: 2026/04/27 07:27:21 by akkim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,4 +54,15 @@ char	*minishell_strrchr(const char *str, int ch)
 		return ((char *)&str[i]);
 	}
 	return (last);
+}
+
+char	*ft_strjoin_free(char *s1, char *s2)
+{
+	char	*new_str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	new_str = ft_strjoin(s1, s2);
+	free(s1);
+	return (new_str);
 }
